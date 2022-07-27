4756ded14606ccc479204dbed7eb884dab63ce01
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-07-27 12:36:34 +0500

    initial commit🚀, add express and boilerplate code

---
+++ README.md
@@ -0,0 +1,5 @@
+# natours ✈
+
+## introduction
+
+This is my implementation of the final 'natours' course project from the udemy course [Node.js, Express, MongoDB & More: The Complete Bootcamp 2022](https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/).
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
