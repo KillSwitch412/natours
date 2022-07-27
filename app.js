bb87020011a08103e212ef0aa5c3bc67336eb33e
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-07-27 16:32:51 +0500

    learn middlewares and add middleware notes�

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
 const express = require('express');
+const morgan = require('morgan');
 const fs = require('fs');
 
 const port = 3000;
 const app = express();
 
-// * seting middleware
-// * app.use is used to set middleware
-//   we need to use middleware bcz express by default
-//   doesn't come with a middleware,
+// ! 1) middlewares
+// * adding middleware
+// * app.use() is used to add middleware
+
+// morgan is a thirdparty logging middleware
+app.use(morgan('dev'));
+
+// we need to use middleware bcz express doesn't
+// set a middleware by default
+// * express.json() here is built into express from the
+// * thirdparty body-parser middleware
 app.use(express.json());
 
+// middleware is a function that is called during the
+// request-response cylce, we can create our own, and also
+// use built in middleware functions,
+// ex: res.send() is also a middleware function
+
+// route handlers ex: app.get(), app.route().get()
+// are also middleware,
+// * thay are the last middleware in a req-res cycle,
+// any middleware declared after them will not be executed.
+// * so, order of middleware matters a lot
+// * this is also know as THE MIDDLEWARE STACK,
+// it is all the middlewares that are executed in a req-res cycle
+
+// * here we are creating our own middleware,
+// it comes with (req, res, next),
+// the next function must be called after executing our code,
+// to call the next middleware
+// otherwise the req-res cycle will get stuck in our function
+
+// here we are adding a property requestTime on our req object,
+// the next middleware that will be called after this middleware
+// will have access to this property
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+// ! 2) route handlers
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -17,6 +53,7 @@ const tours = JSON.parse(
 const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: { tours },
     });
@@ -104,6 +141,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// ! 3) routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app.route('/api/v1/tours/:id')
@@ -111,6 +149,7 @@ app.route('/api/v1/tours/:id')
     .patch(updateTour)
     .delete(deleteTour);
 
+// ! 4) start server
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
 });
