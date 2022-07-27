045bbd1814b8be5297ab25aef54df6d823a27cc7
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-07-28 02:07:58 +0500

    add mongoose, connect to cloud database, add tour schema and model, and some test documents

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// specifying our config file before requiring app file
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DBconnectionString = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DBconnectionString)
+    .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    price: { type: String, required: [true, 'A tour must have a price'] },
+    rating: { type: Number, default: 4.5 },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+// const testTour = new Tour({
+//     name: 'The Park Camper',
+//     price: 997,
+// });
+
+// testTour
+//     .save()
+//     .then((doc) => {
+//         console.log(doc);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
