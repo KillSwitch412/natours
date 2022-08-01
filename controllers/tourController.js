d996c11d74d4f2999b219a79e272efc8e922eed9
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-08-01 13:00:15 +0500

    add pagination to api, but not working correctly

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,10 +34,16 @@ exports.getAllTours = async (req, res) => {
         }
 
         // 4) Pagination
+        // ? pagination not working correctly
         const page = req.query.page * 1 || 1;
         const limit = req.query.limit * 1 || 100;
         const skip = (page - 1) * limit;
         query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page does not exist');
+        }
 
         // * EXECUTE QUERY
         const tours = await query;
