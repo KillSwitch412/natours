1da7317438f952c697064ed8c25ae29fa205c891
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-08-02 15:14:08 +0500

    handling uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! --> Shutting Down...');
+
+    console.log(`ERROR: ${err.name}`);
+    console.log(`MESSAGE: ${err.message}`);
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
