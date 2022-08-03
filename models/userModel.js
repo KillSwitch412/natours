c223a9df631c57c08d19e781163ba006d69bdd44
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-08-03 20:50:01 +0500

    create user schema and model, add password encryption with bcrypt

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter name to create account'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter email to create account'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email address'],
+    },
+    photo: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password to create an account'],
+        minLength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // this only works on .CREATE or .SAVE, not update
+            validator: function (pw) {
+                return pw === this.password;
+            },
+            message: 'Please enter matching passwords',
+        },
+    },
+});
+
+// pre-save middleware:
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
